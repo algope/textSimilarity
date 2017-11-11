5bf43579f584084af678e24377e3f316bee0ddeb
Alejandro González <alejandro.gon.per@gmail.com>
2017-11-11 17:56:29 +0100

    Cleanup

--- main.py
+++ main.py
@@ -4,26 +4,27 @@ from min_hashing import MinHashing
 from compare_signatures import CompareSignatures
 
 if __name__ == "__main__":
+    """Main function of our experiment.
+    It runs all the classes implemented using the data stored in the folder "/data" inside the project.
+    This data folder contains extracts from news related with different topics. The first line is the headline."""
+
+    # Generates the dictionary with shingles from all documents.
     shingling = Shingling(1)
     shingles = shingling.run()
 
-    # print(shingles['340.txt'])
-    # print(shingles['341.txt'])
-
+    # Computes the Jaccard similarity of two given documents.
     comparison = CompareSets(shingles['340.txt'], shingles['341.txt'])
     similarity = comparison.run()
-    print("Similarity is: %s" % similarity)
+    print("Jaccard similarity is: %s" % similarity)
 
+    # Builds the minHash of two given documents and a given length.
     min_hashing = MinHashing(shingles['340.txt'], 100)
     min_hashing2 = MinHashing(shingles['340.txt'], 100)
-
     val1 = min_hashing.run()
     val2 = min_hashing2.run()
 
+    # Estimates similarity of two minhash signatures as a fraction of components, in which they agree.
     compare_signatrues = CompareSignatures(val1, val2)
     compared = compare_signatrues.run()
-    print("Estimation: %s" % compared)
-
-
-
+    print("Estimation of the compared signatures: %s" % compared)
 
--- shingling.py
+++ shingling.py
@@ -8,6 +8,9 @@ from itertools import islice, chain
 
 
 class Shingling:
+    """class Shingling that constructs k–shingles of a given length k (e.g., 10) from a given document, computes a
+           hash value for each unique shingle, and represents the document in the form of an ordered set of its hashed
+           k-shingles. """
 
     def __init__(self, length):
         self.k = length
@@ -33,59 +36,35 @@ class Shingling:
         return result
 
     def shingling(self):
-        """class Shingling that constructs k–shingles of a given length k (e.g., 10) from a given document, computes a
-        hash value for each unique shingle, and represents the document in the form of an ordered set of its hashed
-        k-shingles. """
-
-        # The current shingle ID value to assign to the next new shingle we
-        # encounter. When a shingle gets added to the dictionary, we'll increment this
-        # value.
-        curShingleID = 0
-
-        # Create a dictionary of the articles, mapping the article identifier (e.g.,
-        # "t8470") to the list of shingle IDs that appear in the document.
+        # Dictionary to store collections of shingles per document.
         global_shingles = {}
-
-        doc_names = []
-        num_shingles = 0
 
         for file in os.listdir('./data'):
 
-            with codecs.open('./data/'+file, encoding="utf-8") as f:
+            with codecs.open('./data/' + file, encoding="utf-8") as f:
 
-                # Read all of the words (they are all on one line) and split them by white
-                # space.
+                # Read all lines (except first one since it is the heading of the article)
                 lines = f.readlines()[1:]
                 doc_in_lines = []
                 for line in lines:
                     if line is not '\n':
                         doc_in_lines.append(line)
 
-                # Retrieve the article ID, which is the first word on the line.
+                # Get the file name for indexing in the dictionary.
                 file_id = str(file)
-                # Maintain a list of all document IDs.
-                doc_names.append(file_id)
 
-                # 'shinglesInDoc' will hold all of the unique shingle IDs present in the
-                # current document. If a shingle ID occurs multiple times in the document,
-                # it will only appear once in the set (this is a property of Python sets).
+                # Storing the shingles as a set
                 shingles = set()
 
-                # Construct the shingle text by combining three words together.
                 for line in doc_in_lines:
 
                     shingle = self.get_shingles(line, self.k)
-                    # Hash the shingle to a 32-bit integer.
 
                     for shin in shingle:
-
+                        # Hashing and storing the shingle
                         crc = binascii.crc32(shin.encode('utf-8')) & 0xffffffff
-                        # Add the hash value to the list of shingles for the current document.
-                        # Note that set objects will only add the value to the set if the set
-                        # doesn't already contain it.
                         shingles.add(crc)
 
-                # Store the completed list of shingles for this document in the dictionary.
                 global_shingles[file_id] = shingles
 
         return global_shingles
