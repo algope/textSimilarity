2b349a94b7651ca6516ed5ccc618696978a64b9a
Alejandro González <alejandro.gon.per@gmail.com>
2017-11-11 18:46:36 +0100

    more verbose on main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from shingling import Shingling
 from compare_sets import CompareSets
 from min_hashing import MinHashing
 from compare_signatures import CompareSignatures
+from timeit import default_timer as timer
 
 if __name__ == "__main__":
     """Main function of our experiment.
@@ -9,22 +10,37 @@ if __name__ == "__main__":
     This data folder contains extracts from news related with different topics. The first line is the headline."""
 
     # Generates the dictionary with shingles from all documents.
+    print(">>> Generating shingles for all documents...")
     shingling = Shingling(1)
+    start = timer()
     shingles = shingling.run()
+    end = timer()
+    print(">>> Shingles generated in %.2f secs" % (end-start))
+    print("")
 
     # Computes the Jaccard similarity of two given documents.
     comparison = CompareSets(shingles['340.txt'], shingles['341.txt'])
+    start = timer()
     similarity = comparison.run()
-    print("Jaccard similarity is: %s" % similarity)
+    end = timer()
+    print(">>>>>> Jaccard similarity is: %s and it took: %.2f secs" % (similarity, (end-start)))
+    print("")
 
     # Builds the minHash of two given documents and a given length.
+    print(">>> Building the minHash for two given documents.")
     min_hashing = MinHashing(shingles['340.txt'], 100)
-    min_hashing2 = MinHashing(shingles['340.txt'], 100)
+    min_hashing2 = MinHashing(shingles['341.txt'], 100)
+    start = timer()
     val1 = min_hashing.run()
     val2 = min_hashing2.run()
+    end = timer()
+    print(">>> minHashes built in %.2f secs" % (end - start))
+    print("")
 
     # Estimates similarity of two minhash signatures as a fraction of components, in which they agree.
     compare_signatrues = CompareSignatures(val1, val2)
+    start = timer()
     compared = compare_signatrues.run()
-    print("Estimation of the compared signatures: %s" % compared)
+    end = timer()
+    print(">>>>>> Estimation of the compared signatures: %s and it took %.2f secs" % (compared, (end-start)))
 
