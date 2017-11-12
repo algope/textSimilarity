870b7af4c97bc9751a6669aafab5a77f61066599
Alejandro González <alejandro.gon.per@gmail.com>
2017-11-12 13:06:27 +0100

    LSH

---
+++ lsh.py
@@ -0,0 +1,51 @@
+import numpy as np
+from operator import itemgetter
+from compare_signatures import CompareSignatures
+
+
+class LSH:
+
+    def __init__(self, minHash, t):
+        self._minHash = minHash
+        self._t = t
+
+    def run(self):
+        return self.apply_LSH_technique(self._minHash, self._t)
+
+    def initialize_array_bucket(self, bands):
+        array_buckets = []
+        for band in range(bands):
+            array_buckets.append([[] for i in range(101)])
+        return array_buckets
+
+    def apply_LSH_technique(self, minHash, t):
+
+        bands = 4
+        rows = 3
+
+        array_buckets = self.initialize_array_bucket(bands)
+
+        candidates = {}
+
+        i = 0
+        for b in range(bands):
+            buckets = array_buckets[b]
+            band = minHash[i:i + rows, :]
+            for col in range(band.shape[1]):
+                key = int(sum(band[:, col]) % len(buckets))
+                buckets[key].append(col)
+            i = i + rows
+
+            for item in buckets:
+                if len(item) > 1:
+                    pair = (item[0], item[1])
+                    if pair not in candidates:
+                        A = minHash[:, item[0]]
+                        B = minHash[:, item[1]]
+                        compare_signatures = CompareSignatures(A, B)
+                        similarity = compare_signatures.run()
+                        if similarity >= t:
+                            candidates[pair] = similarity
+
+        sort = sorted(candidates.items(), key=itemgetter(1), reverse=True)
+        return candidates, sort
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from compare_sets import CompareSets
 from min_hashing import MinHashing
 from compare_signatures import CompareSignatures
 from timeit import default_timer as timer
+from lsh import LSH
+import numpy as np
 
 if __name__ == "__main__":
     """Main function of our experiment.
@@ -30,12 +32,26 @@ if __name__ == "__main__":
     print(">>> Building the minHash for two given documents.")
     min_hashing = MinHashing(shingles['340.txt'], 100)
     min_hashing2 = MinHashing(shingles['341.txt'], 100)
+    min_hashing3 = MinHashing(shingles['342.txt'], 100)
+    min_hashing4 = MinHashing(shingles['343.txt'], 100)
+    min_hashing5 = MinHashing(shingles['344.txt'], 100)
+    min_hashing6 = MinHashing(shingles['345.txt'], 100)
+    min_hashing7 = MinHashing(shingles['346.txt'], 100)
+    min_hashing8 = MinHashing(shingles['347.txt'], 100)
     start = timer()
     val1 = min_hashing.run()
     val2 = min_hashing2.run()
+    val3 = min_hashing3.run()
+    val4 = min_hashing4.run()
+    val5 = min_hashing5.run()
+    val6 = min_hashing6.run()
+    val7 = min_hashing7.run()
+    val8 = min_hashing8.run()
+
     end = timer()
     print(">>> minHashes built in %.2f secs" % (end - start))
     print("")
+
 
     # Estimates similarity of two minhash signatures as a fraction of components, in which they agree.
     compare_signatrues = CompareSignatures(val1, val2)
@@ -43,4 +59,25 @@ if __name__ == "__main__":
     compared = compare_signatrues.run()
     end = timer()
     print(">>>>>> Estimation of the compared signatures: %s and it took %.2f secs" % (compared, (end-start)))
+    print("")
 
+
+    print(">>> Calculating LSH...")
+    # Estimating similarity of a group of documents using LSH
+    a1 = np.array(val1)
+    a2 = np.array(val2)
+    a3 = np.array(val3)
+    a4 = np.array(val4)
+    a5 = np.array(val5)
+    a6 = np.array(val6)
+    a7 = np.array(val7)
+    a8 = np.array(val8)
+
+    matrix = np.column_stack((a1, a2, a3, a4, a5, a6, a7, a8))
+    lsh = LSH(matrix, 0.8)
+    candidates, sort = lsh.run()
+
+    print(">>>>>> Similar pair of documents:")
+    for i in range(len(sort)):
+        pair = sort[i][0]
+        print(pair)
